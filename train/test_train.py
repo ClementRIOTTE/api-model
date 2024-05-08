fa28851632bdfd16682b2778229b6c244cb99ee1
ClementRIOTTE <clement.riotte@student-cs.fr>
2024-05-08 12:02:28 +0100

    Update test_train.py

--- train/test_train.py
+++ train/test_train.py
@@ -5,5 +5,5 @@ class TestTrain(TestCase)
     def test_clean_data(self):
         df = pd.DataFrame({'survived' :  [1,0,1],'pclass':[1,2,3],'sex':["male","female","male"], 'age':[20,30,40]})
         result_df = clean_data(df)
-        self.assertEqual(result_df.shape[0],)
-        self.assertEqual(result_df['sex'],)
+        self.assertEqual(result_df.shape[0],3)
+        self.assertEqual(result_df['sex'][0],0)
